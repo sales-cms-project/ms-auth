9883e556adae88f273cabda22f8cfb3b14d067d7
Tiago Cambará <tc@learningbank.io>
2023-09-27 19:44:10 +0100

    feat(sc-124): setup the inital config for grpc ms

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import {
+  AUTH_SERVICE_NAME,
+  AuthServiceController,
+  TokenDto,
+} from './domain/proto/auth.pb';
+import { Observable } from 'rxjs';
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController implements AuthServiceController {
+  @GrpcMethod(AUTH_SERVICE_NAME)
+  signup(): TokenDto {
+    return {
+      token: 'new Token',
+    };
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  signin(): TokenDto | Promise<TokenDto> | Observable<TokenDto> {
+    throw new Error('Method not implemented.');
+  }
+
+  @GrpcMethod(AUTH_SERVICE_NAME)
+  deleteAuthCredential(): void {
+    console.log('foiii');
+  }
+
+  changeAuthCredential(): TokenDto | Promise<TokenDto> | Observable<TokenDto> {
+    throw new Error('Method not implemented.');
+  }
+
+  checkToken(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  restartToken(): TokenDto | Promise<TokenDto> | Observable<TokenDto> {
+    throw new Error('Method not implemented.');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { protobufPackage } from './domain/proto/auth.pb';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: `localhost:3003`,
+      protoPath: join(
+        'node_modules/@sales-cms-project/lib-proto/proto/auth.proto',
+      ),
+      package: protobufPackage,
+    },
+  });
+  await app.listen();
 }
 bootstrap();
