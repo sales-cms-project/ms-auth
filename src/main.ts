e1a4d29b378525085c3c8cd146805c0e932e7b11
Tiago Cambará <tc@learningbank.io>
2023-09-28 17:20:16 +0100

    feat(sc-124): implement the new setup for app start up

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
-import { join } from 'path';
-import { protobufPackage } from './domain/proto/auth.pb';
+import { loadMicroservice } from './config/microservice.loader';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.GRPC,
-    options: {
-      url: `localhost:3003`,
-      protoPath: join(
-        'node_modules/@sales-cms-project/lib-proto/proto/auth.proto',
-      ),
-      package: protobufPackage,
-    },
-  });
+  const app = await loadMicroservice();
   await app.listen();
 }
 bootstrap();
